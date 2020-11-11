3e4213f0f0fdec53b20f3dbf075fd2bb70e1b3d0
admin-45053 <admin-45053@admin-45053-PC>
2020-11-11 19:15:07 +0200

    Domino

--- src/edu/school/DominoTile.java
+++ src/edu/school/DominoTile.java
@@ -1,5 +1,50 @@
 package edu.school;
 
 public class DominoTile {
+	private int left;
+	private int right;
+	
+	public DominoTile(int left, int right) {
+		if(left >=0 && left <=6 && right >= 0 && right <= 6) {
+			this.left = left;
+			this.right = right;
+		}
+	}
+	public DominoTile() {
+		this.left = 0;
+		this.right = 0;
+	}
+	
+	public int getLeft() {
+		return this.left;
+	}
+	public int getRight() {
+		return this.right;
+	}
+	
+	public void swapValues() {
+		int temp = left;
+		left = right;
+		right = temp;
+	}
+	public boolean equals(DominoTile anotherDomino) {
+		if(getLeft() == anotherDomino.getLeft() && getRight() == anotherDomino.getRight()) {
+			return true;
+		}
+		if(getLeft() == anotherDomino.getRight() && getRight() == anotherDomino.getLeft()) {
+			return true;
+		}
+			return false;
+	}
+	public String toString() {
+		char[][] arrayDomino = new char[3][6];
+		for(int row = 0; row < arrayDomino.length ; row++) {
+			for(int col = 0; col < arrayDomino.length; col++) {
+				
+			}
+		}
+		return "";
+	}
+	
 
 }
